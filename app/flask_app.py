9409979030e9299b15ffb2ea00fb49df6d0e1ea4
kerolos <kerolos.sss@gmail.com>
2020-09-12 16:13:08 +0200

    making the filters work, with a littile validation demo

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,12 +1,12 @@
 # mongo.py
-
+import enum
 from flask import Flask
 from flask import jsonify
 from flask import request
 from pymongo.database import Database
 from bson import ObjectId
 from bson.json_util import dumps, loads 
-
+from schema import Schema, And, Use, Optional
 
 # from flask_pymongo import PyMongo
 
@@ -20,9 +20,78 @@ db = None #type: Database
 
 # mongo = PyMongo(app)
 
-@app.route('/coffee-machines', methods=['GET'])
-def get_coffee_machines():
-  coffee_machines = db.products.find({"type": "COFFEE_MACHINE"})
+class BasicTypes(enum.Enum):
+  COFFEE_MACHINE = "COFFEE_MACHINE"
+  COFFEE_POD = "COFFEE_POD"
+
+# just to figure out how it works
+# for weekday in (BasicTypes):
+#    print(weekday)
+#    print(weekday.value)
+
+@app.route('/test', methods=['GET'])
+def test():
+  condition = {"type": "COFFEE_MACHINE"}
+  output = _get_matching_products(condition)
+  return jsonify({'results' : output})
+
+
+@app.route('/coffee-machines', methods=['POST'])
+def filter_coffee_machines():
+  input_json = request.json
+  schema = Schema({
+    ## seems there is a adjustment needed to the error messages 
+    ## TODO: I can't worry about that now
+    Optional('product_type'): And(str, lambda x : x in [
+      'COFFEE_MACHINE_SMALL',
+      'COFFEE_MACHINE_LARGE',
+      'ESPRESSO_MACHINE'
+    ]),
+    Optional('water_line_compatible'):  And(bool),
+    # Optional('model'): And(str, lambda s: s in ['premium', ...] )
+  });
+  schema.validate(input_json)
+
+
+  condition = {}
+  condition.update(input_json)
+  condition.update({"type": "COFFEE_MACHINE"})
+  
+  output = _get_matching_products(condition)
+
+  return jsonify({'results' : output})
+
+
+
+@app.route('/coffee-pods', methods=['POST'])
+def filter_coffee_pods():
+  input_json = request.json
+  schema = Schema({
+    ## seems there is a adjustment needed to the error messages 
+    ## TODO: I can't worry about that now
+    Optional('product_type'): And(str, lambda x : x in [
+      'COFFEE_POD_SMALL',
+      'COFFEE_POD_LARGE',
+      'ESPRESSO_POD'
+    ]),
+    ## TODO: include other attributes' constraints
+    Optional('pack_size'): And(str, len),
+    Optional('coffee_flavor'): And(str, len),
+  });
+  schema.validate(input_json)
+
+
+  condition = {}
+  condition.update(input_json)
+  condition.update({"type": "COFFEE_POD"})
+  
+  output = _get_matching_products(condition)
+
+  return jsonify({'results' : output})
+
+
+def _get_matching_products(condition):
+  coffee_machines = db.products.find(condition)
   output = []
   def process_value(key, val):
     # if val is str or val is bool:
@@ -34,7 +103,7 @@ def get_coffee_machines():
   for c in coffee_machines:
     output.append( {k: process_value(k, v) for k, v in c.items() })
 
-  return jsonify({'result' : output})
+  return output
 
 def run(target_database: Database, **kwargs):
     global db 
